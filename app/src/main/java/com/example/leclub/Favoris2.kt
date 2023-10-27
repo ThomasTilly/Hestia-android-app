127f04c54af556f415ccf598ab73b172af5701e3
TTilly <thomas.tilly@eemi.com>
2023-10-27 04:58:10 +0200

    add more features on "Messages", "product" and "profil" pages

--- app/src/main/java/com/example/leclub/Favoris2.kt
+++ app/src/main/java/com/example/leclub/Favoris2.kt
@@ -5,28 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Favoris2.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Favoris2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +19,23 @@ class Favoris2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favoris2, container, false)
-    }
+        val v = inflater.inflate(R.layout.fragment_favoris2, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Favoris2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Favoris2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val bt1 = v.findViewById<Button>(R.id.viewprod)
+        val bt2 = v.findViewById<Button>(R.id.viewprod2)
+
+        bt1.setOnClickListener {
+            val secondFragment = product()
+            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.frame_layout, secondFragment)
+            transaction.commit()
+        }
+        bt2.setOnClickListener {
+            val secondFragment = product()
+            val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transaction.replace(R.id.frame_layout, secondFragment)
+            transaction.commit()
+        }
+        return v
     }
 }
